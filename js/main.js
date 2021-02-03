6481781b17bc5a7af88e18f646203e463673abc6
Alexandre <lamontagna@gmail.com>
2021-02-03 01:30:01 -0300

    Aula1_03022021

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Alexandre La Montagna";
+var idade = 49;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
